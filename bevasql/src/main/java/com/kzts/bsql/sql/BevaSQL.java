7960c78bf66a7c075684fc411413ebb23ae6081f
geraschenko <geraschenko@kzts.ru>
2019-07-08 13:02:05 +0500

    add table valued function

--- bevasql/src/main/java/com/kzts/bsql/sql/BevaSQL.java
+++ bevasql/src/main/java/com/kzts/bsql/sql/BevaSQL.java
@@ -3,6 +3,11 @@ package com.kzts.bsql.sql;
 public class BevaSQL {
     private ConnectionManager connectionManager;
 
+    public BevaSQL fromValues(String server, String database, String user, String password) {
+        ConnectionToken token = new ConnectionToken(server, database, user, password);
+        return new BevaSQL(token);
+    }
+
     public BevaSQL(ConnectionToken token) {
         this.connectionManager = ConnectionManager.getFromToken(token);
     }
